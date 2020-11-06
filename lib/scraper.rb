4037284ad921e282ad3806c5cd0f58b5fced2ade
Mimi-12 <oneiconforlife26@gmail.com>
2020-11-06 14:30:13 -0800

    commit tree ed91665bd66e1def9018dcc78064307727be823b

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,6 +1,6 @@
 require 'nokogiri'
 require 'open-uri'
-
+require 'pry'
 require_relative './course.rb'
 
 class Scraper
@@ -15,8 +15,19 @@ class Scraper
       end
     end
   end
-  
+
+  def get_page
+    doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses")) 
+    doc.css(".post").each do |post|
+      course = Course.new
+      course.title = post.css("h2").text
+      course.schedule = post.css(".date").text
+      course.description = post.css("p").text
+  binding.pry
+  end
 end
+end
+
 
 
 
